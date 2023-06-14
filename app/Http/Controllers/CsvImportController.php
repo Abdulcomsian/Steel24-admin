34e920de041ecd65a7bfef5e38fafe3ce9f64e81
zeshanrabnawaz008 <zeshan.rabnawaz@accrualhub.com>
2023-06-14 18:12:57 +0500

    Import file changes

--- app/Http/Controllers/CsvImportController.php
+++ app/Http/Controllers/CsvImportController.php
@@ -16,47 +16,56 @@ class CsvImportController extends Controller
 
     public function import(Request $request)
     {
-        $excel_import = Excel::import(new LotsImport(), $request->file('csv_file'));
-        dd("done");
-        // dd($excel_import);
-        // if ($request->hasFile('csv_file')) {
-        //     $path = $request->file('csv_file')->getRealPath();
-        //     // dd($path);
-        //     $data = Excel::toArray([], $path);
-        //     // dd($data);
-        //     if (!empty($data)) {
-        //         foreach ($data[0] as $index=>$row) {
-        //             if($row[0]=="Lot No")
-        //             {
-        //                continue;
-        //             }
-        //             if($index==1)
-        //             {
-        //                 $start_date = date('Y-m-d H:m:i', strtotime($row[8]));
-        //                 $end_date = date('Y-m-d H:m:i', strtotime($row[9]));
-        //             }
-        //             lots::create([
-        //                 'title' => $row[0],
-        //                 'Seller' => $row[1],
-        //                 'Plant' => $row[2],
-        //                 'materialLocation' => $row[3],
-        //                 'description' => $row[6],
-        //                 'Quantity' => $row[7],
-        //                 'StartDate' => $start_date,
-        //                 'EndDate' => $end_date,
-        //                 'Price' => $row[10],
-        //             ]);
-        //             // if($index==1){
-        //             //  exit;
-        //             // }
-        //         }
-        //     }
-            // dd()
-        //     $lotsData = lots::all();
-
-        //     return view('admin.lots.import-csv', compact('lotsData'));
-        // }
-
-        return "No CSV file found.";
+        try {
+            Excel::import(new LotsImport(), $request->file('csv_file'));
+    
+            // dd("done");
+            // dd($excel_import);
+            // if ($request->hasFile('csv_file')) {
+            //     $path = $request->file('csv_file')->getRealPath();
+            //     // dd($path);
+            //     $data = Excel::toArray([], $path);
+            //     // dd($data);
+            //     if (!empty($data)) {
+            //         foreach ($data[0] as $index=>$row) {
+            //             if($row[0]=="Lot No")
+            //             {
+            //                continue;
+            //             }
+            //             if($index==1)
+            //             {
+            //                 $start_date = date('Y-m-d H:m:i', strtotime($row[8]));
+            //                 $end_date = date('Y-m-d H:m:i', strtotime($row[9]));
+            //             }
+            //             lots::create([
+            //                 'title' => $row[0],
+            //                 'Seller' => $row[1],
+            //                 'Plant' => $row[2],
+            //                 'materialLocation' => $row[3],
+            //                 'description' => $row[6],
+            //                 'Quantity' => $row[7],
+            //                 'StartDate' => $start_date,
+            //                 'EndDate' => $end_date,
+            //                 'Price' => $row[10],
+            //             ]);
+            //             // if($index==1){
+            //             //  exit;
+            //             // }
+            //         }
+            //     }
+                // dd()
+            //     $lotsData = lots::all();
+    
+            //     return view('admin.lots.import-csv', compact('lotsData'));
+            // }
+    
+            return "CSV file is found.";
+        } 
+        catch (\Throwable $e) 
+        {
+            return "CSV file is not found.";
+        }
     }
 }
+
+
--- app/Imports/LotsImport.php
+++ app/Imports/LotsImport.php
@@ -6,6 +6,7 @@ use DateTime;
 use Carbon\Carbon;
 use App\Models\lots;
 use App\Models\materials;
+use App\Models\categories;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Validators\Failure;
@@ -24,54 +25,96 @@ class LotsImport implements WithHeadingRow, SkipsOnFailure, ToCollection
     {
         foreach ($rows as $row) 
         {
-            if(is_numeric($row['lot_no']))
+            if (is_numeric($row['lot_no']))
             {
                 $lot = new lots();
+                $lot->title = $row['lot_no'] ?? '';
                 $lot->description = $row['description'] ?? '';
+                $lot->materialLocation = $row['material_location'] ?? '';
+                $lot->Price = $row['start_price'] ?? '';
                 $lot->Seller = $row['seller'] ?? '';
-                $lot->Category = $row['category'] ?? '';
-                $lot->Plant    = $row['plant'];
-                $lot->Quantity    = $row['quantity'];
-                $lot->StartDate    = (date('Y-m-d H:m:i', strtotime($row['start_date'])));
-                $lot->EndDate    = (date('Y-m-d H:m:i', strtotime($row['end_date'])));
-                $lot->save();
-            }
-            if($row['lot_no']=='Batch No')
-            {
-                continue;
-            }
-            if($row['lot_no'] == "Lot No")
-            {
-                continue;
-            }
-            if(is_numeric($row['lot_no']))
-            {
-                continue;
+
+                // Check if the category exists
+                $category = categories::where('title', $row['category'])->first();
+
+                if (!$category) {
+                    // Category does not exist, create a new one
+                    $category = new categories();
+                    $category->title = $row['category'];
+                    // Set other category fields here
+                    $category->save();
+                   
+                }
+                $lot->categoryId = $category->id;
+                $lot->Plant = $row['plant'];
+                $lot->Quantity = $row['quantity'];
+                $lot->StartDate = (date('Y-m-d H:i:s', strtotime($row['start_date'])));
+                $lot->EndDate = (date('Y-m-d H:i:s', strtotime($row['end_date'])));
+
+                // If the category doesn't exist, set the categoryId field
+                if (!$category) {
+                    $lot->save();
+                } else {
+                    // Category exists, don't set the categoryId field
+                    $lot->saveQuietly();
+                }
+
+                // previous code ither dev
+                // if($row['lot_no']=='Batch No')
+                // {
+                //     continue;
+                // }
+                // if($row['lot_no'] == "Lot No")
+                // {
+                //     continue;
+                // }
+                // if(is_numeric($row['lot_no']))
+                // {
+                //     continue;
+                    
+                // }
+                // if(is_null($row['lot_no']))
+                // {
+                //     continue;
+                // }
+
+
+                $material = new materials();
+                $material->width = $row['product'] ?? '';
+                $material->plantNo = $row['16'] ?? '';
+                $material->qty = $row['end_date'] ?? '';
+                $material->grade = $row['quantity'] ?? '';
+                $material->JSWgrade = $row['description'] ?? '';
+                $material->coilLength = $row['category'] ?? '';
+                $material->tinTemper = $row['material'] ?? '';
+                $material->passivation = $row['14'] ?? '';
+                $material->coldTreatment = $row['15'] ?? '';
+                $material->storageLocation = $row['17'] ?? '';
+                $material->plantLotNo = $row['18'] ?? '';
+                $material->lot_id = $lot->id;
+                $material->save();
                 
-            }
-            if(is_null($row['lot_no']))
-            {
-                continue;
+                // This code is also useable 
+                // $material = new materials();
+                // $material->width = $row['4'] ?? '';
+                // $material->coilLength = $row['5'] ?? '';
+                // $material->JSWgrade = $row['6'] ?? '';
+                // $material->grade = $row['7'] ?? '';
+                // $material->qty = $row['8'] ?? '';
+                // $material->tinTemper = $row['11'] ?? '';
+                // $material->passivation = $row['14'] ?? '';
+                // $material->coldTreatment = $row['15'] ?? '';
+                // $material->plantNo = $row['16'] ?? '';
+                // $material->storageLocation = $row['17'] ?? '';
+                // $material->plantLotNo = $row['18'] ?? '';
+                // $material->lot_id = $lot->id;
+                // $material->save();   
+
             }
             
-            $material = new materials();
-            $material->thick = $row['material_location'] ?? '';
-            $material->width = $row['product'] ?? '';
-            $material->plantNo = $row['16'] ?? '';
-            $material->qty = $row['end_date'] ?? '';
-            $material->grade = $row['quantity'] ?? '';
-            $material->JSWgrade = $row['description'] ?? '';
-            $material->coilLength = $row['category'] ?? '';
-            $material->tinTemper = $row['material'] ?? '';
-            $material->passivation = $row['14'] ?? '';
-            $material->coldTreatment = $row['15'] ?? '';
-            $material->storageLocation = $row['17'] ?? '';
-            $material->plantLotNo = $row['18'] ?? '';
-            $material->lot_id = $lot->id;
-            $material->save();
-        }        
+            // Handle other conditions and continue the loop...
+        }
     }
-
 
     public function onFailure(Failure ...$failures)
     {
