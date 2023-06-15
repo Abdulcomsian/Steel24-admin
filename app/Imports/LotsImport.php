3ee7b08bdf2c5cfd8fa5cad55fe072e0cba5b3b9
ZeeshanAlamgir <zeeshan.alamgir@tangenttek.com>
2023-06-15 10:01:43 +0500

    import lots and materials final commit

--- app/Imports/LotsImport.php
+++ app/Imports/LotsImport.php
@@ -2,19 +2,14 @@
 
 namespace App\Imports;
 
-use DateTime;
-use Carbon\Carbon;
 use App\Models\lots;
 use App\Models\materials;
 use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Validators\Failure;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\SkipsFailures;
 use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithValidation;
 
 class LotsImport implements WithHeadingRow, SkipsOnFailure, ToCollection
 {
@@ -27,13 +22,15 @@ class LotsImport implements WithHeadingRow, SkipsOnFailure, ToCollection
             if(is_numeric($row['lot_no']))
             {
                 $lot = new lots();
-                $lot->description = $row['description'] ?? '';
-                $lot->Seller = $row['seller'] ?? '';
-                $lot->Category = $row['category'] ?? '';
-                $lot->Plant    = $row['plant'];
-                $lot->Quantity    = $row['quantity'];
-                $lot->StartDate    = (date('Y-m-d H:m:i', strtotime($row['start_date'])));
-                $lot->EndDate    = (date('Y-m-d H:m:i', strtotime($row['end_date'])));
+                $lot->description   = $row['description'] ?? 'NULL';
+                $lot->uid           = $row['lot_no'] ?? 'NULL';
+                $lot->Seller        = $row['seller'] ?? 'NULL';
+                $lot->Category      = $row['category'] ?? 'NULL';
+                $lot->Plant         = $row['plant'] ?? 'NULL';
+                $lot->Quantity      = $row['quantity'] ?? 'NULL';
+                $lot->lot_status    = "Pending";
+                $lot->StartDate     = (date('Y-m-d H:m:i', strtotime($row['start_date']))) ?? 'NULL';
+                $lot->EndDate       = (date('Y-m-d H:m:i', strtotime($row['end_date']))) ?? 'NULL';
                 $lot->save();
             }
             if($row['lot_no']=='Batch No')
@@ -53,21 +50,27 @@ class LotsImport implements WithHeadingRow, SkipsOnFailure, ToCollection
             {
                 continue;
             }
-            
             $material = new materials();
-            $material->thick = $row['material_location'] ?? '';
-            $material->width = $row['product'] ?? '';
-            $material->plantNo = $row['16'] ?? '';
-            $material->qty = $row['end_date'] ?? '';
-            $material->grade = $row['quantity'] ?? '';
-            $material->JSWgrade = $row['description'] ?? '';
-            $material->coilLength = $row['category'] ?? '';
-            $material->tinTemper = $row['material'] ?? '';
-            $material->passivation = $row['14'] ?? '';
-            $material->coldTreatment = $row['15'] ?? '';
-            $material->storageLocation = $row['17'] ?? '';
-            $material->plantLotNo = $row['18'] ?? '';
-            $material->lot_id = $lot->id;
+            $material->title            = $row['seller'] ?? 'NULL';
+            $material->uid              = $row['lot_no'] ?? 'NULL';
+            $material->thick            = $row['material_location'] ?? 'NULL';
+            $material->width            = $row['product'] ?? 'NULL';
+            $material->plantNo          = $row['16'] ?? 'NULL';
+            $material->qty              = $row['end_date'] ?? 'NULL';
+            $material->grade            = $row['quantity'] ?? 'NULL';
+            $material->JSWgrade         = $row['description'] ?? 'NULL';
+            $material->coilLength       = $row['category'] ?? 'NULL';
+            $material->tinTemper        = $row['material'] ?? 'NULL';
+            $material->passivation      = $row['14'] ?? 'NULL';
+            $material->coldTreatment    = $row['15'] ?? 'NULL';
+            $material->storageLocation  = $row['17'] ?? 'NULL';
+            $material->plantLotNo       = $row['18'] ?? 'NULL';
+            $material->qualityRemark    = $row['24'] ?? 'NULL';
+            $material->heatNo           = $row['22'] ?? 'NULL';
+            $material->majorDefect      = $row['21'] ?? 'NULL';
+            $material->coating          = $row['start_price'] ?? 'NULL';
+            $material->eqSpeci          = $row['auction'] ?? 'NULL';
+            $material->lot_id           = $lot->id;
             $material->save();
         }        
     }
---
+++ storage/framework/views/53bbe8cc13a437e4492face1f915fb4724e68196.php
@@ -0,0 +1,108 @@
+
+<?php $__env->startSection('content'); ?>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header card-header-primary">
+                                    <h4 class="card-title">Complete Lots</h4>
+                                </div>
+                                <div class="card-body">
+                                    <?php if(session('success')): ?>
+                                        <div class="alert alert-success" role="success">
+                                            <?php echo e(session('success')); ?>
+
+                                        </div>
+                                    <?php endif; ?>
+
+                               
+
+                                    <div > <!-- remove class="table-responsive" -->
+                                        <table class="table data-table table-striped">
+                                            <thead class="text-primary text-center">
+                                                <th>ID</th>
+                                                <th>Title</th>
+                                                <th>Start date</th>
+                                                <th>End date</th>
+                                                <th>Status</th>
+                                                <th>Actions</th>
+                                            </thead>
+                                            <tbody class="text-center">
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="card-footer mr-auto">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<style>
+    .sorting:before,
+    .sorting_asc:before,
+    .sorting_desc:before,
+    .sorting:after,
+    .sorting_asc:after,
+    .sorting_desc:after,
+    .sorting::after {
+        display: none !important;
+    }
+</style>
+<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var table = $('.data-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "<?php echo route('admin.expirelots'); ?>",
+
+            columns: [{
+                    data: 'id',
+                    name: 'id'
+                },
+                {
+                    data: 'title',
+                    name: 'title'
+                },
+                {
+                    data: 'StartDate',
+                    name: 'StartDate'
+                },
+
+                {
+                    data: 'EndDate',
+                    name: 'EndDate'
+                },
+                {
+                    data: 'lot_status',
+                    name: 'lot_status'
+                },
+                {
+                    data: null,
+                    render: function(data, type, row) {
+                        return (`<div><a href="<?php echo e(url('admin/completelotbids/${data.id}')); ?>"class="btn btn-info" style="padding-top:20px"><i class="material-icons">person</i></a>
+                    `);
+                    },
+                }
+            ]
+        });
+    });
+</script>
+
+<?php echo $__env->make('admin.layouts.main', ['activePage' => '', 'titlePage' => 'Lots'], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Steel24-admin\resources\views/admin/lots/completeIndex.blade.php ENDPATH**/ ?>
