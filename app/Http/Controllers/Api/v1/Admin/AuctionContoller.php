ca0b86e896b8b86f28a3cefc77f7505ff70840b0
nraccrual <nauman.riaz@accrualhub.com>
2023-08-09 11:12:47 +0500

    route name issue

--- app/Http/Controllers/Api/v1/Admin/AuctionContoller.php
+++ app/Http/Controllers/Api/v1/Admin/AuctionContoller.php
@@ -2380,12 +2380,13 @@ class AuctionContoller extends Controller
 
         $autoBid =AutoBid::where(['lotId' => $lotId , 'customerId' => $customerId])->get();
 
-        dd($autoBid);
-
+        
         if(AutoBid::where(['lotId' => $lotId , 'customerId' => $customerId])->count())
         {
+            dd("inside if" , AutoBid::where(['lotId' => $lotId , 'customerId' => $customerId])->count() );
             return response()->json(['success' => true , 'msg' => 'An autobid has been placed against this customer' , 'autobid' => 1]);
         }else{
+            dd("inside else" , AutoBid::where(['lotId' => $lotId , 'customerId' => $customerId])->count());
             return response()->json(['success' => false , 'msg' => 'There is no auto bid related to this customer' , 'autobid' => 0]);
         }
 
