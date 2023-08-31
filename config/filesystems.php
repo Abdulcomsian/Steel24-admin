a2ecfe3cd04de3a817ff92a581f3708ffe05f254
zeshanrabnawaz008 <zeshan.rabnawaz@accrualhub.com>
2023-08-31 09:59:52 +0500

    update code filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,22 +32,22 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => public_path('public'),
+            'root' => storage_path('app'),
         ],
 
         // 'public' => [
         //     'driver' => 'local',
-        //     'root' => storage_path('app/public'),
+        //     'root' => public_path(''),
         //     'url' => env('APP_URL').'/storage',
         //     'visibility' => 'public',
-        // ],
+        // ], 
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path( ),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-        ], 
+        ],
 
         'ExcelLots' => [
             'driver' => 'local',
