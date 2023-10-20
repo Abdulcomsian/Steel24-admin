4c8b43e3c584a4fd31294a0862e82ea41c065a8e
nraccrual <nauman.riaz@accrualhub.com>
2023-10-20 15:46:03 +0530

    winning lot issue resolved

--- app/Http/Controllers/Api/v1/Admin/LotsContoller.php
+++ app/Http/Controllers/Api/v1/Admin/LotsContoller.php
@@ -2123,8 +2123,6 @@ class LotsContoller extends Controller
         $startDate = $request->input('start_date');
         $endDate = $request->input('end_date');
         $customerId = $request->input('customer_id');
-
-        dd($startDate , $endDate , $customerId );
     
         // $customerLots = CustomerLot::with(['lotDetail.materials', 'lot.materials'])
         //     ->whereBetween(DB::raw('Date(customer_lots.created_at)'), [$startDate, $endDate])
@@ -2140,11 +2138,13 @@ class LotsContoller extends Controller
         });
 
         $customerLots->when(isset($startDate) && !is_null($startDate) , function($query) use ($startDate){
-            $query->where(DB::raw('DATE(created_at)') , '<=' , $startDate);
+            $query->where('created_at' , '>=' , $startDate);
         });
 
+  
+
         $customerLots->when(isset($endDate) && !is_null($endDate) , function($query) use ($endDate){
-            $query->where(DB::raw('DATE(created_at)') , '>=' , $endDate);
+            $query->where(DB::raw('DATE(created_at)') , '<=' , $endDate);
         });
 
         $customerLots->where('customer_id' , $customerId);
